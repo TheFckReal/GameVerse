f7a2b81bce3fe13b0ee751e1773506fcc9bdb9a2
The Real <sashasan2003@mail.ru>
2024-04-13 03:34:13 +0300

    feature: Добавлен кэширование сообщений от рекомендательной системы

---
+++ GameVerse_recommendation/Services/RecommendationsCache.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace GameVerse_recommendation.Services
+{
+    public class RecommendationsCache : MemoryCache
+    {
+        public RecommendationsCache(IOptions<MemoryCacheOptions> optionsAccessor) : base(optionsAccessor)
+        {
+        }
+
+        public RecommendationsCache(IOptions<MemoryCacheOptions> optionsAccessor, ILoggerFactory loggerFactory) : base(optionsAccessor, loggerFactory)
+        {
+        }
+    }
+}
